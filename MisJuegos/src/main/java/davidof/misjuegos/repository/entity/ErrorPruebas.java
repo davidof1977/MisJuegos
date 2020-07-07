cf6fd6090d4bae7371da226d5bdf9178fd96a836
David <davidof1977@hotmail.com>
2020-07-07 17:55:47 +0200

    Added maven plugin

---
+++ MisJuegos/src/main/java/davidof/misjuegos/repository/entity/ErrorPruebas.java
@@ -0,0 +1,18 @@
+package davidof.misjuegos.repository.entity;
+
+public class ErrorPruebas {
+private String description;
+private Integer codigo;
+public String getDescription() {
+	return description;
+}
+public void setDescription(String description) {
+	this.description = description;
+}
+public Integer getCodigo() {
+	return codigo;
+}
+public void setCodigo(Integer codigo) {
+	this.codigo = codigo;
+}
+}
