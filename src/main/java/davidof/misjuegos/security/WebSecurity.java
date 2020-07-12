a70421cc1b778fee6e363298aa08bbb687308684
David <davidof1977@hotmail.com>
2020-07-12 09:35:00 +0200

    Usuario

--- src/main/java/davidof/misjuegos/security/WebSecurity.java
+++ src/main/java/davidof/misjuegos/security/WebSecurity.java
@@ -69,7 +69,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 		final CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("HEAD",
-                "GET", "POST", "PUT", "DELETE", "PATCH"));
+                "GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS"));
         // setAllowCredentials(true) is important, otherwise:
         // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
         configuration.setAllowCredentials(true);
