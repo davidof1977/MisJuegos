696cbc51ad9b97dfdcae0cb641e40fcae8541a66
david <david@DESKTOP-R284CQD>
2021-05-07 09:56:40 +0200

    junit

---
+++ src/main/java/davidof/misjuegos/pruebas/Prueba1.java
@@ -0,0 +1,9 @@
+package davidof.misjuegos.pruebas;
+
+public class Prueba1 {
+	
+	public boolean getPrueba1() {
+		return true;
+	}
+
+}
---
+++ src/test/java/davidof/misjuegos/pruebas/TestPruebas.java
@@ -0,0 +1,15 @@
+package davidof.misjuegos.pruebas;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestPruebas {
+
+	@Test
+	public void test() {
+		Prueba1 p = new Prueba1();
+		Assert.assertTrue("OK", p.getPrueba1());
+	}
+
+}
